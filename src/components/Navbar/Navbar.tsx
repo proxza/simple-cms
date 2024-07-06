ef9495709fc8a0ff72fd26ee0c4f45e1a0a1f737
proxza <proxza@gmail.com>
2024-07-06 12:38:12 +1000

    fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar: React.FC = () => {
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex>logo</Flex>
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="logo" height="30px" />
+        <Image src="/images/redditText.svg" alt="logo-2" height="46px" display={{ base: "none", md: "unset" }} />
+      </Flex>
     </Flex>
   );
 };
